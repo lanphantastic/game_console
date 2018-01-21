29ed390f899705b2ff3337710c0781b3200b5d21
Lan Phan <lanc_phan@hotmail.com>
2018-01-21 17:34:15 -0500

    create an index.html.erb and added an additional route 'delete' to products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products do
+    get "delete"
+  end
+
+  root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
